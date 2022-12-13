773aa45c29595a110fd361d94c4616a18de95792
ElPettego <mattiapapaccioli@gmail.com>
2022-12-13 12:58:40 +0100

    sml fix

--- telegram_message_bot.py
+++ telegram_message_bot.py
@@ -1,41 +1,28 @@
-import logging
 from telegram.ext import Updater
 import time
 import telegram
 
 class TelegramBot:
-
     def __init__(self, chat_id, bot_token):
         self.chat_id = chat_id
         self.bot_token = bot_token
-        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                            level=logging.INFO)
-        logger = logging.getLogger(__name__)   
-                
+        self.updater = Updater(self.bot_token, use_context=True)
+        dp = self.updater.dispatcher
+        self.bot = telegram.Bot(token=self.bot_token)
+
     def emit(self, mex):
-
-        updater = Updater(self.bot_token, use_context=True)
-        dp = updater.dispatcher
-        bot = telegram.Bot(token=self.bot_token)
-
-
-        status = bot.send_message(chat_id=self.chat_id, text=mex, parse_mode=telegram.ParseMode.HTML, disable_web_page_preview=True)
+        status = self.bot.send_message(chat_id=self.chat_id, text=mex, parse_mode=telegram.ParseMode.HTML, disable_web_page_preview=True)
+        self.updater.start_polling()
         # time.sleep(0.2)
-        # print(status)
-        updater.start_polling()
-        # time.sleep(0.2)
-        updater.stop() 
+        self.updater.stop() 
         
     def send_file(self, path_to_file):
-        updater = Updater(self.bot_token, use_context=True)
-        dp = updater.dispatcher
-        bot = telegram.Bot(token=self.bot_token)
-        status = bot.send_document(chat_id=self.chat_id, 
+        status = self.bot.send_document(chat_id=self.chat_id, 
                                    document=open(path_to_file, 'rb'),
                                    filename=path_to_file,
                                    parse_mode=telegram.ParseMode.HTML)
         time.sleep(0.2)
         print(f'INVIANDO FILE A {self.chat_id}.')
-        updater.start_polling()
+        self.updater.start_polling()
         time.sleep(0.2)
-        updater.stop()
+        self.updater.stop()
